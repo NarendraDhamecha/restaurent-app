ffe4c7f1cab5035b67f7ad402e82acd43cc7f434
NarendraDhamecha <nikhildhamecha19@gmail.com>
2023-01-24 09:20:49 +0530

    adding_a_form

--- src/components/Meals/MealList.js
+++ src/components/Meals/MealList.js
@@ -1,19 +1,22 @@
 import React from "react";
-import classes from './MealList.module.css';
+import classes from "./MealList.module.css";
+import MealListForm from "./MealListForm";
 
 const MealList = (props) => {
+  const price = `$${props.price.toFixed(2)}`;
 
-    const price = `$${props.price.toFixed(2)}`
-
-    return (
-        <li className={classes.meal}>
-            <div>
-                <h3>{props.name}</h3>
-                <div className={classes.description}>{props.description}</div>
-                <div className={classes.price}>{price}</div>
-            </div>
-        </li>
-    )
-}
+  return (
+    <li className={classes.meal}>
+      <div>
+        <h3>{props.name}</h3>
+        <div className={classes.description}>{props.description}</div>
+        <div className={classes.price}>{price}</div>
+      </div>
+      <div>
+        <MealListForm />
+      </div>
+    </li>
+  );
+};
 
 export default MealList;
---
+++ src/components/Meals/MealListForm.js
@@ -0,0 +1,23 @@
+import classes from "./MealListForm.module.css";
+import Input from "../UI/Input";
+
+const MealListForm = (props) => {
+  return (
+    <form className={classes.form}>
+      <Input
+        label="Amount"
+        input={{
+          id: "Amount",
+          type: "number",
+          min: "1",
+          max: "5",
+          step: "1",
+          defaultValue: "1",
+        }}
+      />
+      <button>+ Add</button>
+    </form>
+  );
+};
+
+export default MealListForm;
