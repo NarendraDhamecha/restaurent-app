83170dee66905df8866bcef94e620cd723c5e333
NarendraDhamecha <nikhildhamecha19@gmail.com>
2023-01-24 21:34:01 +0530

    making_a_hard_coded_cart_without_any_functionality

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,21 @@
+import React, { Fragment } from "react";
+import Modal from "../UI/Modal";
+import classes from './Cart.module.css';
+
+const Cart = (props) => {
+  return (
+    <Modal>
+      Sushi
+      <div className={classes.total}>
+        <span>Amount</span>
+        <span>24.90</span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes['button--alt']}>Close</button>
+        <button className={classes.button}>Open</button>
+      </div>
+    </Modal>
+  );
+};
+
+export default Cart;
